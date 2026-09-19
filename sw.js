fca661e296709cac05b96f45b96de3a5af9b7a19
pjg605-debug <pjg605@gmail.com>
2026-09-20 08:09:38 +0900

    Publish clinical brief 2026-09-20: posterior knee pain in KOA

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const VERSION="clinical-reasoning-v4";
+const VERSION="clinical-reasoning-v5";
 const CORE=[
   "./","./index.html","./assets/styles.css","./assets/app.js","./articles.json","./offline.html",
   "./assets/icons/icon-192.png","./assets/icons/icon-512.png",
+  "./articles/2026-09-20-knee-oa-posterior-pain.html",
   "./articles/2026-09-19-lateral-elbow.html",
   "./articles/2026-09-18-plantar.html","./articles/2026-09-18-shoulder.html"
 ];
