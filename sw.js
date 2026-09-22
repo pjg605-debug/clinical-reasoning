8ba9269d66e51a859d057360395d39e1526096fa
pjg605-debug <pjg605@gmail.com>
2026-09-23 08:37:10 +0900

    Publish clinical brief 2026-09-23: greater trochanteric pain syndrome

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-const VERSION="clinical-reasoning-v13";
+const VERSION="clinical-reasoning-v14";
 const CORE=[
   "./","./index.html","./assets/styles.css","./assets/app.js","./articles.json","./offline.html","./manifest.webmanifest",
   "./assets/icons/icon-192.png","./assets/icons/icon-512.png",
+  "./articles/2026-09-23-gtps-lateral-hip.html",
   "./articles/2026-09-22-walking-leg-pain-stenosis.html",
   "./articles/2026-09-21-acute-neck-arm-tingling.html",
   "./articles/2026-09-20-knee-oa-posterior-pain.html",
   "./articles/2026-09-19-lateral-elbow.html",
   "./articles/2026-09-18-plantar.html","./articles/2026-09-18-shoulder.html",
+  "./assets/briefs/2026-09-23-gtps-anatomy-painmap.svg","./assets/briefs/2026-09-23-gtps-exam-sequence.svg",
   "./assets/briefs/2026-09-22-lss-painmap.svg","./assets/briefs/2026-09-22-lss-walk-test.svg","./assets/briefs/2026-09-22-lss-outcomes.svg",
   "./assets/briefs/2026-09-21-neck-painmap.svg","./assets/briefs/2026-09-21-neck-exam-sequence.svg",
   "./assets/briefs/plantar-painmap-v4.jpg","./assets/briefs/plantar-exams-v4.jpg",
@@ -26,7 +28,6 @@ self.addEventListener("activate",event=>{
   );
 });
 
-// Keep successful responses only; a temporary HTTP error must not poison offline data.
 async function networkResponse(req,cacheKey=req){
   const res=await fetch(req);
   if(!res.ok)throw new Error("HTTP "+res.status);
